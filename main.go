de953a62baf9c9214e7987ad7023fe4dd7afd486
Daniel Hao Huynh <huynhdaniel10@gmail.com>
2022-01-25 17:17:47 +0100

    AverageNumber

--- main.go
+++ main.go
@@ -9,25 +9,38 @@ import (
 func main() {
 	var flagValidity bool
 	flag.BoolVar(&flagValidity, "test", false, "takes the string as an input, and returns a boolean value if it complies with the format 23-ab-48-caba-56-haha")
+	var flagAverage bool
+	flag.BoolVar(&flagAverage, "avr", false, "takes the string, and returns the average number from all the numbers")
 
 	flag.Parse()
 
-	if flagValidity {
-		testString := []string{
-			"23-ab-48-caba-56-haha",
-			"",
-			"420-this works-1337-hi-69",
-			"ab-12-cava-41-lol-32",
-			"-12-ab-",
-		}
-		var i int
-		for i = 0; i < len(testString); i++ {
+	testString := []string{
+		"23-ab-48-caba-56-haha",
+		"",
+		"420-this works-1337-hi-69",
+		"ab-12-cava-41-lol-32",
+		"-12-ab-",
+	}
+
+	fmt.Print("\n\n")
+	var i int
+	for i = 0; i < len(testString); i++ {
+		if flagValidity {
 			if assignment1.TestValidity(testString[i]) {
 				fmt.Println("[Success]", "('", testString[i], "')")
+				if flagAverage {
+					fmt.Print("\t")
+					assignment1.AverageNumber(testString[i])
+
+				}
 			} else {
 				fmt.Println("[Failure]", "('", testString[i], "')")
 			}
+			fmt.Print("\n")
+		} else {
+			if flagAverage {
+				assignment1.AverageNumber(testString[i])
+			}
 		}
 	}
-
 }
--- pckg/functions.go
+++ pckg/functions.go
@@ -1,5 +1,11 @@
 package assignment1
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 /**
  *  Test Validity
  *  takes the string as an input, and returns boolean flag true if the given string complies with the format,
@@ -37,4 +43,54 @@ func TestValidity(inn string) bool {
 		}
 	}
 	return true
+}
+
+/**
+ *  Average Number
+ *  takes the string, and returns the average number from all the numbers
+ *
+ *  @param inn  the string it takes in as an input
+ */
+func AverageNumber(inn string) uint {
+	if len(inn) < 1 { // if string is empty
+		return 0
+	}
+	if inn[0] == '-' || inn[len(inn)-1] == '-' { // if string starts or ends with '-' dash
+		return 0
+	}
+	var i int
+	j := 0 // counter for bool array containing the states of the strings
+	isNum := make([]bool, len(inn))
+	for i = 0; i < len(inn); i++ {
+		if inn[i] != '-' { // skips check for '-'
+			isNum[j] = true // automatically sets it to number
+			if !(uint(inn[i]) >= 48 && uint(inn[i]) <= 57) {
+				isNum[j] = false // switches if it isnt a number
+			}
+		} else {
+			j++ // changes check to next string
+		}
+	}
+	strInn := strings.Split(inn, "-")
+	if isNum[0] == false { // if first string is NAN
+		return 0
+	}
+	var avgNum uint
+	div := uint(0)
+	fmt.Print("The average number of these numbers:[")
+	for i = 0; i < j; i++ {
+		if isNum[i] == true { // if its a number
+			tNum, err := strconv.Atoi(strInn[i]) // convert from ascii to integer
+			if err != nil {
+				fmt.Println(err)
+				return 0
+			}
+			fmt.Print(" ", tNum, " ")
+			avgNum += uint(tNum) // adds it to the answer
+			div++
+		}
+	}
+	fmt.Print("] is ")
+	fmt.Println((avgNum / div))
+	return (avgNum / div) // divides the answer by count of numbers and returns it
 }
