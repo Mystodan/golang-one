382fcaa714f5b48a7d3c97b0a15cf24ab20d3c2d
Daniel Hao Huynh <huynhdaniel10@gmail.com>
2022-01-30 17:29:13 +0100

    generate function and tweaks

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"flag"
 	"fmt"
 	assignment1 "golang1/pckg"
+	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	var flagValidity bool
 	flag.BoolVar(&flagValidity, "test", false, "takes the string as an input, and returns a boolean value if it complies with the format 23-ab-48-caba-56-haha")
 	var flagAverage bool
@@ -15,19 +19,47 @@ func main() {
 	flag.BoolVar(&flagWStory, "story", false, "takes the string, and replaces the numbers with ' 'spaces")
 	var flagSStats bool
 	flag.BoolVar(&flagSStats, "stats", false, "takes the string, and returns four things: the shortest- longest words, in addition to the average wordlength and lists all the words with average w. len.(rounded)")
+	var flagGenTrue bool
+	flag.BoolVar(&flagGenTrue, "gen_true", false, " returns a string value which complies with the format f. ex. 23-ab-48-caba-56-haha")
+	var flagGenFalse bool
+	flag.BoolVar(&flagGenFalse, "gen_false", true, "returns a string value which does not complies with the format f. ex. -av-a2-48-caba-56-haha")
 
 	flag.Parse()
 
-	testString := []string{
-		"23-ab-48-caba-56-haha",
-		"",
-		"420-this works-1337-hi-69",
-		"ab-12-cava-41-lol-32",
-		"-12-ab-",
-		"1-hello-2-world",
+	var testString []string
+	if len(flag.Arg(0)) > 0 {
+		testString = []string{
+			"23-ab-48-caba-56-haha",
+			"",
+			"420-this works-1337-hi-69",
+			"ab-12-cava-41-lol-32",
+			"-12-ab-",
+			"1-hello-2-world",
+			assignment1.Generate(true),
+			assignment1.Generate(false),
+			assignment1.Generate(true),
+			flag.Arg(0),
+		}
+	} else {
+		testString = []string{
+			"23-ab-48-caba-56-haha",
+			"",
+			"420-this works-1337-hi-69",
+			"ab-12-cava-41-lol-32",
+			"-12-ab-",
+			"1-hello-2-world",
+			assignment1.Generate(true),
+			assignment1.Generate(false),
+			assignment1.Generate(true),
+		}
 	}
 
 	fmt.Print("\n\n")
+	if flagGenTrue {
+		fmt.Println(assignment1.Generate(true))
+	} else {
+		fmt.Println(assignment1.Generate(false))
+	}
 	var i int
 	for i = 0; i < len(testString); i++ {
 		if flagValidity {
--- pckg/functions.go
+++ pckg/functions.go
@@ -3,6 +3,7 @@ package assignment1
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"strconv"
 	"strings"
 )
@@ -52,46 +53,36 @@ func TestValidity(inn string) bool {
  *  @param inn  the string it takes in as an input
  */
 func AverageNumber(inn string) uint {
-	if len(inn) < 1 { // if string is empty
-		return 0
-	}
-	if inn[0] == '-' || inn[len(inn)-1] == '-' { // if string starts or ends with '-' dash
-		return 0
-	}
-	j := 0 // counter for bool array containing the states of the strings
-	isNum := make([]bool, uint(len(inn)/3))
-	for i := 0; i < len(inn); i++ {
-		if inn[i] != '-' { // skips check for '-'
-			isNum[j] = true // automatically sets it to number
-			if !(uint(inn[i]) >= 48 && uint(inn[i]) <= 57) {
-				isNum[j] = false // switches if it isnt a number
+	if TestValidity(inn) {
+		j := 0 // counter for bool array containing the states of the strings
+		isNum := make([]bool, uint(len(inn)/3))
+		for i := 0; i < len(inn); i++ {
+			if inn[i] != '-' { // skips check for '-'
+				isNum[j] = true // automatically sets it to number
+				if !(uint(inn[i]) >= 48 && uint(inn[i]) <= 57) {
+					isNum[j] = false // switches if it isnt a number
+				}
+			} else {
+				j++ // changes check to next string
 			}
-		} else {
-			j++ // changes check to next string
 		}
-	}
-	strInn := strings.Split(inn, "-")
-	if isNum[0] == false { // if first string is NAN
-		return 0
-	}
-	var avgNum uint
-	div := uint(0)
-	fmt.Print("The average number of the numbers:[")
-	for i := 0; i < j; i++ {
-		if isNum[i] == true { // if its a number
-			tNum, err := strconv.Atoi(strInn[i]) // convert from ascii to integer
-			if err != nil {
-				fmt.Println(err)
-				return 0
+		strInn := strings.Split(inn, "-")
+		var avgNum uint
+		div := uint(0)
+		fmt.Print("The average number of the numbers:[")
+		for i := 0; i < j; i++ {
+			if isNum[i] == true { // if its a number
+				tNum, _ := strconv.Atoi(strInn[i]) // convert from ascii to integer
+				fmt.Print(" ", tNum, " ")
+				avgNum += uint(tNum) // adds it to the answer
+				div++
 			}
-			fmt.Print(" ", tNum, " ")
-			avgNum += uint(tNum) // adds it to the answer
-			div++
 		}
+		fmt.Print("] is ")
+		fmt.Println((avgNum / div))
+		return (avgNum / div) // divides the answer by count of numbers and returns it
 	}
-	fmt.Print("] is ")
-	fmt.Println((avgNum / div))
-	return (avgNum / div) // divides the answer by count of numbers and returns it
+	return 0
 }
 
 /**
@@ -103,43 +94,38 @@ func AverageNumber(inn string) uint {
  *  @param inn  the string it takes in as an input
  */
 func WholeStory(inn string) string {
-	if len(inn) < 1 { // if string is empty
-		return ""
-	}
-	if inn[0] == '-' || inn[len(inn)-1] == '-' { // if string starts or ends with '-' dash
-		return ""
-	}
-	j := 0 // counter for bool array containing the states of the strings
-	isNum := make([]bool, uint(len(inn)/3))
-	for i := 0; i < len(inn); i++ {
-		if inn[i] != '-' { // skips check for '-'
-			isNum[j] = true // automatically sets it to number
-			if !(uint(inn[i]) >= 48 && uint(inn[i]) <= 57) {
-				isNum[j] = false // switches if it isnt a number
-			}
-		} else {
-			j++ // changes check to next string
-		}
-	}
-	strInn := strings.Split(inn, "-")
-	if isNum[0] == false { // if first string is NAN
-		return ""
-	}
-	var rString string
-	fmt.Print("The string with numbers replaced :[ ")
-	for i := 0; i <= j; i++ {
-		if isNum[i] == true { // if its a number
-			if i != 0 && i != j {
-				strInn[i] = " "
+	if TestValidity(inn) {
+		j := 0 // counter for bool array containing the states of the strings
+		isNum := make([]bool, uint(len(inn)/3))
+		for i := 0; i < len(inn); i++ {
+			if inn[i] != '-' { // skips check for '-'
+				isNum[j] = true // automatically sets it to number
+				if !(uint(inn[i]) >= 48 && uint(inn[i]) <= 57) {
+					isNum[j] = false // switches if it isnt a number
+				}
 			} else {
-				strInn[i] = ""
+				j++ // changes check to next string
 			}
 		}
-		rString += strInn[i]
+		strInn := strings.Split(inn, "-")
+
+		var rString string
+		fmt.Print("The string with numbers replaced :[ ")
+		for i := 0; i <= j; i++ {
+			if isNum[i] == true { // if its a number
+				if i != 0 && i != j {
+					strInn[i] = " "
+				} else {
+					strInn[i] = ""
+				}
+			}
+			rString += strInn[i]
+		}
+		fmt.Print(rString)
+		fmt.Println(" ]")
+		return (rString) // the string with dashes replaced
 	}
-	fmt.Print(rString)
-	fmt.Println(" ]")
-	return (rString) // the string with dashes replaced
+	return ""
 }
 
 /**
@@ -153,80 +139,157 @@ func WholeStory(inn string) string {
  *  @param inn  the string it takes in as an input
  */
 func StoryStats(inn string) (string, string, float64, []string) {
-	if len(inn) < 1 { // if string is empty
-		return "", "", 0.0, []string{""}
-	}
-	if inn[0] == '-' || inn[len(inn)-1] == '-' { // if string starts or ends with '-' dash
-		return "", "", 0.0, []string{""}
-	}
-	j := 0 // counter for bool array containing the states of the strings
-	isNum := make([]bool, uint(len(inn)/3))
-	for i := 0; i < len(inn); i++ {
-		if inn[i] != '-' { // skips check for '-'
-			isNum[j] = true // automatically sets it to number
-			if !(uint(inn[i]) >= 48 && uint(inn[i]) <= 57) {
-				isNum[j] = false // switches if it isnt a number
-			}
-		} else {
-			j++ // changes check to next string
-		}
-	}
-	// splitting string into strings
-	strInn := strings.Split(inn, "-")
-	if isNum[0] == false { // if first string is NAN
-		return "", "", 0.0, []string{""}
-	}
-	//handler for string length
-	shortestString := strInn[1]
-	longestString := strInn[1]
-	for i := 0; i < len(strInn); i++ {
-		if isNum[i] == false {
-			if len(shortestString) > len(strInn[i]) {
-				shortestString = strInn[i]
-			}
-			if len(longestString) < len(strInn[i]) {
-				longestString = strInn[i]
+	if TestValidity(inn) {
+		j := 0 // counter for bool array containing the states of the strings
+		isNum := make([]bool, uint(len(inn)/3))
+		for i := 0; i < len(inn); i++ {
+			if inn[i] != '-' { // skips check for '-'
+				isNum[j] = true // automatically sets it to number
+				if !(uint(inn[i]) >= 48 && uint(inn[i]) <= 57) {
+					isNum[j] = false // switches if it isnt a number
+				}
+			} else {
+				j++ // changes check to next string
 			}
 		}
-	}
-	//handler for average word length
-	counter := 0
-	averageLength := 0.0
-
-	for i := 0; i < len(strInn); i++ {
-		if isNum[i] == false {
-			averageLength += float64(len(strInn[i])) // adds it to the answer
-			counter++
-		}
-	}
-	if counter > 0 {
-		//	fmt.Print("counter:", averageLength/float64(counter))
-		averageLength = float64(averageLength) / float64(counter)
-	}
-	rString := []string{}
-	// handler for list rounding
-	for i := 0; i < len(strInn); i++ {
-
-		if isNum[i] == false {
-			fmt.Println(averageLength, ":", math.Round(averageLength))
-			if float64(len(strInn[i])) == math.Floor(averageLength) {
-				rString = append(rString, strInn[i])
-			} else if float64(len(strInn[i])) == math.Ceil(averageLength) {
-				rString = append(rString, strInn[i])
+		// splitting string into strings
+		strInn := strings.Split(inn, "-")
+		//handler for string length
+		shortestString := strInn[1]
+		longestString := strInn[1]
+		for i := 0; i < len(strInn); i++ {
+			if isNum[i] == false {
+				if len(shortestString) > len(strInn[i]) {
+					shortestString = strInn[i]
+				}
+				if len(longestString) < len(strInn[i]) {
+					longestString = strInn[i]
+				}
 			}
 		}
-	}
+		//handler for average word length
+		counter := 0
+		averageLength := 0.0
 
-	fmt.Print(
-		"\t-Shortest string: ", shortestString, "\n",
-		"\t-Longest string : ", longestString, "\n",
-		"\t-Average Length : ", averageLength, "\n",
-		"\t-List(between ", math.Floor(averageLength), " - ", math.Ceil(averageLength), "): \n",
-	)
-	for i := 0; i < len(rString); i++ {
-		fmt.Println("\t\t - ", rString[i])
-	}
+		for i := 0; i < len(strInn); i++ {
+			if isNum[i] == false {
+				averageLength += float64(len(strInn[i])) // adds it to the answer
+				counter++
+			}
+		}
+		if counter > 0 {
+			//	fmt.Print("counter:", averageLength/float64(counter))
+			averageLength = float64(averageLength) / float64(counter)
+		}
+		rString := []string{}
+		// handler for list rounding
+		for i := 0; i < len(strInn); i++ {
 
-	fmt.Println()
-	return shortestString, longestString, averageLength, rString // divides the answer by count of numbers and returns it
+			if isNum[i] == false {
+				if float64(len(strInn[i])) == math.Floor(averageLength) {
+					rString = append(rString, strInn[i])
+				} else if float64(len(strInn[i])) == math.Ceil(averageLength) {
+					rString = append(rString, strInn[i])
+				}
+			}
+		}
+
+		fmt.Print(
+			"\t-Shortest string: ", shortestString, "\n",
+			"\t-Longest string : ", longestString, "\n",
+			"\t-Average Length : ", averageLength, "\n",
+			"\t-List(between ", math.Floor(averageLength), " - ", math.Ceil(averageLength), "): \n",
+		)
+		for i := 0; i < len(rString); i++ {
+			fmt.Println("\t\t - ", rString[i])
+		}
+
+		fmt.Println()
+		return shortestString, longestString, averageLength, rString
+	}
+	return "", "", 0.0, []string{""}
+}
+
+/**
+ *  Generate
+ *  takes in a bool, and generates the string if or if not the bool is true, return the string which might comply with the format,
+ *
+ *  @param mode  the boolean it takes in for generation of string
+ */
+func Generate(mode bool) string {
+	var returnString string
+	if mode {
+		var isNum []bool
+		var rString string
+		max := 3
+		min := 1
+		ranNum := (rand.Intn(max-min+1) + min) * 2
+		for i := 0; i < ranNum; i++ {
+			isNum = append(isNum, true)
+			isNum = append(isNum, false)
+		}
+
+		for i := 0; i < len(isNum); i++ {
+			min = 3
+			max = 6
+			if !isNum[i] {
+				for j := 0; j < (rand.Intn(max-min+1) + min); j++ {
+					if rand.Intn(2) > 0 {
+						genNum := (rand.Intn(90-65+1) + 65)
+						rString += string(rune(genNum))
+					} else {
+						genNum := (rand.Intn(122-97+1) + 97)
+						rString += string(rune(genNum))
+					}
+				}
+				if i != len(isNum)-1 {
+					rString += "-"
+				}
+			} else {
+				for j := 0; j < (rand.Intn(max-min+1) + min); j++ {
+					getNum := rand.Intn(57-48+1) + 48
+					rString += string(rune(getNum))
+				}
+				if i != len(isNum)-1 {
+					rString += "-"
+				}
+			}
+		}
+		returnString = rString
+	} else {
+
+		var rString string
+		max := 3
+		min := 1
+		ranNum := (rand.Intn(max-min+1) + min) * 2
+
+		for i := 0; i < ranNum; i++ {
+			min = 3
+			max = 6
+
+			for j := 0; j < (rand.Intn(max-min+1) + min); j++ {
+				if rand.Intn(2) > 0 {
+					genNum := (rand.Intn(90-65+1) + 65)
+					rString += string(rune(genNum))
+				} else {
+					genNum := (rand.Intn(122-97+1) + 97)
+					rString += string(rune(genNum))
+				}
+			}
+			if i != ranNum {
+				rString += "-"
+			}
+
+			for j := 0; j < (rand.Intn(max-min+1) + min); j++ {
+				getNum := rand.Intn(57-48+1) + 48
+				rString += string(rune(getNum))
+			}
+			if i != ranNum {
+				rString += "-"
+			}
+
+		}
+		returnString = rString
+	}
+	return returnString
 }
